c498af6f138809ff2d76c8b0db391fee835ea408
Benjamin Semmler <bsemmler@F3C008.42adel.org.au>
2021-11-08 15:58:21 +1030

    Copied ft_printf

---
+++ libft/ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsemmler <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/15 13:49:06 by bsemmler          #+#    #+#             */
+/*   Updated: 2021/09/27 16:32:06 by bsemmler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*dst;
+	size_t	pos;
+	size_t	length;
+
+	if (s1 == NULL)
+		return (ft_strdup("(null)"));
+	length = ft_strlen(s1);
+	dst = malloc(length);
+	pos = 0;
+	while (pos < length)
+	{
+		dst[pos] = s1[pos];
+		pos++;
+	}
+	dst[pos] = 0;
+	return (dst);
+}
